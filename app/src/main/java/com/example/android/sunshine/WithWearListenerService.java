24eecf3bf7ea86c85f4438f997a41824ef750ab2
Yasuaki <shivaq777@gmail.com>
2017-03-31 14:05:20 +0900

    feat:Add sendMessage function in phone side

--- app/src/main/java/com/example/android/sunshine/WithWearListenerService.java
+++ app/src/main/java/com/example/android/sunshine/WithWearListenerService.java
@@ -4,23 +4,38 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import com.example.android.sunshine.sync.SunshineSyncTask;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.wearable.MessageApi;
 import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Wearable;
+import com.google.android.gms.wearable.WearableListenerService;
 
 import timber.log.Timber;
 
-public class WithWearListenerService implements
-        MessageApi.MessageListener,
+public class WithWearListenerService extends WearableListenerService implements
         GoogleApiClient.OnConnectionFailedListener,
-        GoogleApiClient.ConnectionCallbacks
-{
+        GoogleApiClient.ConnectionCallbacks{
 
+    private static final String START_SYNC_PATH = "/start_sync";
+    GoogleApiClient mGoogleApiClient;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .build();
+        mGoogleApiClient.connect();
+    }
 
     @Override
     public void onMessageReceived(MessageEvent messageEvent) {
 
+        if(messageEvent.getPath().equals(START_SYNC_PATH)){
+            SunshineSyncTask.syncWeather(this);
+        }
     }
 
     @Override
